98bd74c1f3ed67cb188fb1cc071e4d22ec9d1f1c
David Carvajal Garrido <davidcarvajalg@gmail.com>
2021-03-23 23:12:19 +0100

    Mongo Atlas integration

--- server.js
+++ server.js
@@ -5,16 +5,14 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-//const MongoClient = require('mongodb').MongoClient;
-
-const urlDatabase = `${process.env.DB_PROT}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+//const urlDatabase = `${process.env.DB_PROT}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const urlDatabase = process.env.DB_ATLAS;
 const mongoOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true
 };
 
 mongoose.connect(urlDatabase, mongoOptions);
-
 
 // const app = express();
 // app.use(express.static(__dirname + "/build"));
@@ -24,48 +22,70 @@ mongoose.connect(urlDatabase, mongoOptions);
 // app.get("/", function(req, res) {
 // })
 
-async function scraper() {
-  const urlPoem = process.argv[2] || 'https://www.poemas-del-alma.com/el-rey-de-harlem.htm';
-  const html = await axios.get(urlPoem);
-  const $ = await cheerio.load(html.data);
+async function getNextSequenceValue(sequenceName) {
+  let next = 0;
 
-  const title = $('h2.title-poem').text();
+  const CounterSchema = new mongoose.Schema({ _id: String, next: Number }, { versionKey: false });
+
+  CounterSchema.statics.findAndModify = function (query, sort, doc, options, callback) {
+    return this.collection.findAndModify(query, sort, doc, options, callback);
+  };
   
-  let verses = [];
-  let p = 1;
-  let first = true;
+  const Counter = mongoose.model('counters', CounterSchema);
   
-  $('#contentfont p')
-    .html()
-    .split("<br>")
-    .map(line => {
-      const verse = line.trim();
-      
-      if (verse !== "") {
-        if (first) {
-          p = 1;
-          first = false;
-        }
+  await Counter.findAndModify({ _id: sequenceName }, [], { $inc: { next: 1 } }, {}, function (err, counter) {
+    if (err) throw err;
+    next = counter.next;
+  });
 
-        verses.push({ text: verse, paragraph: p });
-      }
-      else
-        p++;
-    });
-    
-    // Insert poem into database
-  const VerseSchema = new mongoose.Schema({ text: String, paragraph: Number }, { _id: false, autoIndex: false });
-  const PoemSchema = new mongoose.Schema({ title: String, verses: [VerseSchema] }, { versionKey: false });
-    
-    const Poem = mongoose.model('poems', PoemSchema);
+  console.log(next)
 
-    const newPoem = new Poem({ title, verses });
-
-    newPoem
-      .save()
-      .then(() => console.log("Ok poema ahora sí que sí"));
+  return next;
 }
 
-scraper();
+async function scraper(urlPoem, idPoem, idBook, idAuthor) {
+  if (urlPoem) {
+    const html = await axios.get(urlPoem);
+    const $ = await cheerio.load(html.data);
+
+    const title = $('h2.title-poem').text();
+    
+    let verses = [];
+    let p = 1;
+    let first = true;
+    
+    $('#contentfont p')
+      .html()
+      .split("<br>")
+      .map(line => {
+        const verse = line.trim();
+        
+        if (verse !== "") {
+          if (first) {
+            p = 1;
+            first = false;
+          }
+
+          verses.push({ text: verse, paragraph: p });
+        }
+        else
+          p++;
+      });
+      
+    // Insert poem into database
+    const VerseSchema = new mongoose.Schema({ text: String, paragraph: Number }, { _id: false, autoIndex: false });
+    const PoemSchema = new mongoose.Schema({ _id: Number, title: String, verses: [VerseSchema], idBook: Number, idAuthor: Number }, { versionKey: false });
+      
+    const Poem = mongoose.model('poems', PoemSchema);
+    //const next = await getNextSequenceValue('idPoem');
+    const newPoem = new Poem({ _id: idPoem, title, verses, idBook, idAuthor })
+    newPoem.save().then(() => console.log("Poema insertado"));
+  }
+  else {
+    console.log("No se indicó URL a scrapear");
+  }
+}
+
+scraper(process.argv[2], 1, 1, 1);
 
 //app.listen(port, () => `Listening on ${port}`);
--- src/providers/PoetryProvider.class.js
+++ src/providers/PoetryProvider.class.js
@@ -4,11 +4,11 @@ export default class PoetryProvider {
   getAuthors() {
     const authors = [
       {
-        id: 1,
+        _id: 1,
         name: "Federico García Lorca"
       },
       {
-        id: 2,
+        _id: 2,
         name: "Rosalía de Castro"
       }
     ];
@@ -19,22 +19,22 @@ export default class PoetryProvider {
   getBooksByAuthor(id) {
     const books = [
       {
-        id: 1,
+        _id: 1,
         title: "Poeta en Nueva York",
         idAuthor: 1
       },
       {
-        id: 2,
+        _id: 2,
         title: "Romancero gitano",
         idAuthor: 1
       },
       {
-        id: 3,
+        _id: 3,
         title: "Cantares gallegos",
         idAuthor: 2
       },
       {
-        id: 4,
+        _id: 4,
         title: "Follas novas",
         idAuthor: 2
       }
@@ -46,92 +46,72 @@ export default class PoetryProvider {
   getPoemsByBook(id) {
     const poems = [
       {
-        id: 1,
-        idBook: 1,
+        _id: 1,
         title: "Poema L1",
-        paragraphs: [
-          [
-            "Verso L111",
-            "Verso L112",
-            "Verso L113"
-          ],
-          [
-            "Verso L121",
-            "Verso L122",
-            "Verso L123"
-          ],
-          [
-            "Verso L131",
-            "Verso L132",
-            "Verso L133"
-          ]
-        ]
+        verses: [
+          "Verso L111",
+          "Verso L112",
+          "Verso L113",
+          "Verso L121",
+          "Verso L122",
+          "Verso L123",
+          "Verso L131",
+          "Verso L132",
+          "Verso L133"
+        ],
+        idBook: 1,
+        idAuthor: 1
       },
       {
-        id: 2,
-        idBook: 2,
+        _id: 2,
         title: "Poema L2",
-        paragraphs: [
-          [
-            "Verso L211",
-            "Verso L212",
-            "Verso L213"
-          ],
-          [
-            "Verso L221",
-            "Verso L222",
-            "Verso L223"
-          ],
-          [
-            "Verso L231",
-            "Verso L232",
-            "Verso L233"
-          ]
-        ]
+        verses: [
+          "Verso L211",
+          "Verso L212",
+          "Verso L213",
+          "Verso L221",
+          "Verso L222",
+          "Verso L223",
+          "Verso L231",
+          "Verso L232",
+          "Verso L233"
+        ],
+        idBook: 2,
+        idAuthor: 1
       },
       {
-        id: 3,
-        idBook: 3,
+        _id: 3,
         title: "Poema R1",
-        paragraphs: [
-          [
-            "Verso R111",
-            "Verso R112",
-            "Verso R113"
-          ],
-          [
-            "Verso R121",
-            "Verso R122",
-            "Verso R123"
-          ],
-          [
-            "Verso R131",
-            "Verso R132",
-            "Verso R133"
-          ]
-        ]
+        verses: [
+          "Verso R111",
+          "Verso R112",
+          "Verso R113",
+          "Verso R121",
+          "Verso R122",
+          "Verso R123",
+          "Verso R131",
+          "Verso R132",
+          "Verso R133"
+        ],
+        idBook: 1,
+        idAuthor: 2
       },
       {
-        id: 4,
-        idBook: 4,
+        _id: 4,
         title: "Poema R2",
-        paragraphs: [
-          [
-            "Verso R211",
-            "Verso R212",
-            "Verso R213"
-          ],
-          [
-            "Verso R221",
-            "Verso R222",
-            "Verso R223"
-          ],
-          [
-            "Verso R231",
-            "Verso R232",
-            "Verso R233"
-          ]
-        ]
+        verses: [
+          "Verso R211",
+          "Verso R212",
+          "Verso R213",
+          "Verso R221",
+          "Verso R222",
+          "Verso R223",
+          "Verso R231",
+          "Verso R232",
+          "Verso R233"
+        ],
+        idBook: 1,
+        idAuthor: 2
       }
     ];
 
